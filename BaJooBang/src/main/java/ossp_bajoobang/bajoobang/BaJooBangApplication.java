091e2f750903e4850bde7ece9045ca79bf527339
deepBeom <i1t28ly@gmail.com>
2024-04-15 19:06:24 +0900

    Configuration

---
+++ BaJooBang/src/main/java/ossp_bajoobang/bajoobang/BaJooBangApplication.java
@@ -0,0 +1,13 @@
+package ossp_bajoobang.bajoobang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaJooBangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaJooBangApplication.class, args);
+    }
+
+}
---
+++ BaJooBang/src/test/java/ossp_bajoobang/bajoobang/BaJooBangApplicationTests.java
@@ -0,0 +1,13 @@
+package ossp_bajoobang.bajoobang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaJooBangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
