994bf2841d67b3b64326bfd0c2340bd42dc0e8b2
(wschoi) <(wsxchoi@gmail.com)>
2024-04-17 14:36:13 +0900

    MemberService에 register(회원가입) 추가

--- BaJooBang/src/main/java/ossp_bajoobang/bajoobang/service/MemberService.java
+++ BaJooBang/src/main/java/ossp_bajoobang/bajoobang/service/MemberService.java
@@ -1,14 +1,35 @@
 package ossp_bajoobang.bajoobang.service;
 
-import lombok.Builder;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ossp_bajoobang.bajoobang.domain.Member;
 import ossp_bajoobang.bajoobang.dto.MemberDTO;
+import ossp_bajoobang.bajoobang.repository.MemberRepository;
 
 @Service
+@RequiredArgsConstructor
 public class MemberService {
+    private final MemberRepository memberRepository;
 
-    public void login(MemberDTO memberDTO){
-//        Member member = Member.builder().pw(memberDTO.)
+    // 회원가입
+    public void register(MemberDTO dto) {
+        Member entity = dtoToEntity(dto);
+        memberRepository.save(entity);
+    }
+
+    /**
+    public void login(MemberDTO dto){
+        Member member = Member.builder().pw(dto.)
+    }
+     */
+
+    // dto를 엔티티로 변환
+    Member dtoToEntity(MemberDTO dto) {
+        Member entity = Member.builder()
+                .id(dto.getId())
+                .email(dto.getEmail())
+                .pw(dto.getPw())
+                .build();
+        return entity;
     }
 }
