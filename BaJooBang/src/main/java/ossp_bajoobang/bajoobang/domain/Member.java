008ddf6425c534c99a941eeab5b8d3bce6280998
wsxchoi <wsxchoi@gmail.com>
2024-04-17 12:59:38 +0900

    Member 엔티티 수정

--- BaJooBang/src/main/java/ossp_bajoobang/bajoobang/domain/Member.java
+++ BaJooBang/src/main/java/ossp_bajoobang/bajoobang/domain/Member.java
@@ -1,8 +1,10 @@
 package ossp_bajoobang.bajoobang.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter @Setter
@@ -12,7 +14,14 @@ import lombok.*;
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
     private String email;
 
     private String pw;
+
+    @OneToMany(mappedBy = "member")
+    private List<Request> request = new ArrayList<>();
 }
