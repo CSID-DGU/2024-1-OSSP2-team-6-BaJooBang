1624312b596057708399848c20015dd82735f4fa
deepBeom <i1t28ly@gmail.com>
2024-04-17 14:27:22 +0900

    add test_sql
    더미 데이터

---
+++ BaJooBang/src/main/resources/query/insert_house.sql
@@ -0,0 +1,6 @@
+insert into house(latitude, longitude, management, money1, money2, size, stair, local_id, content) 
+values (37.561167, 126.995336, 4, 500, 50, 23, 3, 1, "서울특별시 중구 퇴계로 212 (필동2가)"),
+(33.450705, 126.570677, 4, 300, 30, 23, 6, 1, "서울특별시 중구 필동로1길 38"),
+(37.559023, 127.005296, 4, 400, 20, 46, 2, 1, "서울특별시 중구 동호로 지하256");
+ 
+select * from house; 
---
+++ BaJooBang/src/main/resources/query/insert_request.sql
@@ -0,0 +1,5 @@
+insert into request(mold_living, mold_rest, mold_shoes, mold_veranda, mold_window, power_shower, power_wash, power_water, price_request, request_date, lighting, time_water)
+values (1, 0, 0, 0, 1, 1, 0, 2, 4000, '2024-04-14', "good", 32),
+(0, 1, 1, 0, 1, 2, 2, 2, 5000, '2024-04-17', "bad", 42);
+
+select * from request;
