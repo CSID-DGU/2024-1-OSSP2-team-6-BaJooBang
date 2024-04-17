eb0d3bfd8ba2b97db809e3ab07dda8bfa1ab5a2c
(wschoi) <(wsxchoi@gmail.com)>
2024-04-17 14:32:44 +0900

    MemberDTO 수정

--- BaJooBang/src/main/java/ossp_bajoobang/bajoobang/dto/MemberDTO.java
+++ BaJooBang/src/main/java/ossp_bajoobang/bajoobang/dto/MemberDTO.java
@@ -1,13 +1,13 @@
 package ossp_bajoobang.bajoobang.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+@Getter @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class MemberDTO {
+    private Long id;
     private String email;
     private String pw;
 }
