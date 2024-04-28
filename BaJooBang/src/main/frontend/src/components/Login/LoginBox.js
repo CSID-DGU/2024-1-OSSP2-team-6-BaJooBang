dfaf458fca3b0e6fca6a0d9da763e5b98d12c94a
parkjoohyung0826 <dochipjh@gmail.com>
2024-04-28 15:16:58 +0900

    [ADD] 로그인 api 추가

--- BaJooBang/src/main/frontend/src/components/Login/LoginBox.js
+++ BaJooBang/src/main/frontend/src/components/Login/LoginBox.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './box.css';
+import axios from 'axios';
 import { ReactComponent as LoginLeft } from '../images/loginLeft.svg';
 import Input from './Input';
 import { useNavigate } from 'react-router-dom';
@@ -10,6 +11,28 @@ function LoginBox() {
         navigate('/signup'); // 여기서 '/signup'은 LoginPage.js 컴포넌트로 라우팅될 경로입니다.
     };
 
+    const [email, setEmail] = useState('');
+    const [pw, setPw] = useState('');
+
+    async function LoginPost() {
+        const data = {
+            email: email,
+            pw: pw,
+        };
+        try {
+            console.log(email);
+            const response = await axios.post('http://localhost:8000/login', data, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            console.log('Signup success:', response.data);
+            navigate('/login');
+        } catch (error) {
+            console.error('Signup failed:', error);
+        }
+    }
+
     return (
         <div className='box'>
             <div className='leftBox' style={{backgroundColor: '#3CB38F'}}>
@@ -18,9 +41,9 @@ function LoginBox() {
             <div className='rightBox'>
                 <p className='title'>Log In</p>
                 <div className='emptyBox'/>
-                <Input title={"Email Address"} placeholder={'Email Address'}/>
-                <Input title={"Password"} placeholder={'Password'}/>
-                <button className='button' style={{backgroundColor: '#33BE42', marginTop: '40px'}}>Log in</button>
+                <Input title={"Email Address"} placeholder={'Email Address'} onChange={(e) => setEmail(e.target.value)} />
+                <Input title={"Password"} placeholder={'Password'}  onChange={(e) => setPw(e.target.value)}/>
+                <button className='button' style={{backgroundColor: '#33BE42', marginTop: '40px'}} onClick={LoginPost}>Log in</button>
                 <div className='line'/>
                 <button className='bottomButton' onClick={handleSignUpClick}>Sign up</button>
             </div>
