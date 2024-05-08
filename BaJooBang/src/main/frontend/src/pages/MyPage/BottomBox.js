1d9daa6247a1510fc9a2bacf5f7adc8fa88a9100
parkjoohyung <dochipjh@gmail.com>
2024-05-09 00:31:25 +0900

    [ADD] 마이페이지 하단 바 모드 change 추가

--- BaJooBang/src/main/frontend/src/pages/MyPage/BottomBox.js
+++ BaJooBang/src/main/frontend/src/pages/MyPage/BottomBox.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './BottomBox.css'; 
 
-function BottomBox({content, number}) {
+function BottomBox({content, number, Icon}) {
     return (
       <div className="MBBox">
         <div className='MBTextBox'>
             <p className='MBText'>{content}</p>
             <p className='MBText'>{number + '건'}</p>
         </div>
+        <div className='MBImage'>
+            {Icon && <Icon />}
+        </div>
+        
       </div>
     );
   }
