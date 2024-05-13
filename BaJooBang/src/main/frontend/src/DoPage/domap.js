d9351d3941cb6c660eae9122613b1ea8c3794399
youmeanit <umin030312@naver.com>
2024-05-13 21:05:41 +0900

    발품지도 검색기능 추가

--- BaJooBang/src/main/frontend/src/DoPage/domap.js
+++ BaJooBang/src/main/frontend/src/DoPage/domap.js
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './domap.css';
 import DopageMap from './showmap';
 import { ReactComponent as Closer } from '../components/images/closer.svg';
 
-
-
 // 매물지도의 검색라인
-const SearchBar = () => {
+const SearchBar = ({ onSearchChange }) => {
   return (
     <div className="search">
       <label htmlFor="searchfor">매물 검색<span className='blank'></span>|</label><span className='blank'></span>
-      <div className='searchbox'><Closer/><input type="text" placeholder="매물을 검색하세요" /></div>
+      <div className='searchbox'>
+        <Closer />
+        <input 
+          type="text" 
+          placeholder="매물을 검색하세요" 
+          onChange={e => onSearchChange(e.target.value)} 
+        />
       </div>
+    </div>
   );
 };
 
 const Dopage = () => {
+  const [search, setSearch] = useState('');
+
+  const handleSearchChange = (newSearch) => {
+    setSearch(newSearch);
+  };
+
   return (
     <div className='screen'>
-      <SearchBar />
-      <DopageMap/>
+      <SearchBar onSearchChange={handleSearchChange} />
+      <DopageMap search={search} />
     </div>
   );
 };
--- BaJooBang/src/main/frontend/src/DoPage/showmap.js
+++ BaJooBang/src/main/frontend/src/DoPage/showmap.js
@@ -1,122 +1,148 @@
 /*global kakao*/
-import React, { useEffect, useState } from "react";
-import {Link} from 'react-router-dom';
-import axios from 'axios';
-import './showmap.css'
+import React, { useEffect } from "react";
+import { Link } from 'react-router-dom';
+import './showmap.css';
 
-export const dopositions=[ 
+export const dopositions = [
   {
     "house_id": 1,
-    "dealmoney" : "20,000",
-    "time":10,
+    "dealmoney": "20,000",
+    "time": 10,
     "distance": 765,
     "human": "홍길동",
-    "content": "서울특별시 양천구",
+    "content": "서울특별시 필동",
     "latLng": {
-        "lat": 37.558077,
-        "lng": 127.000882
+      "lat": 37.558077,
+      "lng": 127.000882
     }
-}
-  ];
-
-  function DONav(props){
-    return (
-      <nav>
-        <ol>
-          {dopositions.map(position => (
-            <li key={position.house_id}>
-              <h2>W {position.dealmoney}</h2>
-              <p><span className="blank"></span>위치 | {position.content}</p>
-              <p><span className="blank"></span>
-              요청인<span className="blank"></span>|<span className="blank"></span>{position.human}</p>
-              <p><span className="blank"></span>도보 | <span className="blank_gray">{position.time}분</span><span className="blank"></span>
-               거리 | <span className="blank_gray">{position.distance}m</span></p>
-              
-              
-              <Link to={`/`}><span className="blank"></span>요청서 보러가기 {'>>'}</Link>
-            </li>
-          ))}
-        </ol>
-      </nav>
-    );
+  },
+  {
+    "house_id": 2,
+    "dealmoney": "10,000",
+    "time": 10,
+    "distance": 765,
+    "human": "안녕",
+    "content": "서울특별시 을지로",
+    "latLng": {
+      "lat": 37.566882968825,
+        "lng": 126.99092687291
+    }
+  },
+  {
+    "house_id": 3,
+    "dealmoney": "30,000",
+    "time": 10,
+    "distance": 765,
+    "human": "김유민",
+    "content": "서울특별시 장충로",
+    "latLng": {
+      "lat": 37.560413254084,
+      "lng": 127.00768457766
+    }
   }
+];
 
-const DopageMap = () => {
+function DONav({ positions }) {
+  return (
+    <nav>
+      <ol>
+        {positions.map(position => (
+          <li key={position.house_id}>
+            <h2>￦ {position.dealmoney}</h2>
+            <p><span className="blank"></span>위치 | {position.content}</p>
+            <p><span className="blank"></span>요청인<span className="blank"></span>|<span className="blank"></span>{position.human}</p>
+            <p><span className="blank"></span>도보 | <span className="blank_gray">{position.time}분</span><span className="blank"></span>거리 | <span className="blank_gray">{position.distance}m</span></p>
+            <Link to={`/`}><span className="blank"></span>요청서 보러가기 {'>>'}</Link>
+          </li>
+        ))}
+      </ol>
+    </nav>
+  );
+}
 
-  useEffect(() => { 
+const DopageMap = ({ search }) => {
+  const filteredPositions = dopositions.filter(position =>
+    position.content.includes(search)
+  );
+
+  useEffect(() => {
     // 마커를 담을 배열입니다
-    try {
+    let markers = [];
+    let mapContainer = document.getElementById("map"); // 지도를 표시할 div
 
-      var mapContainer = document.getElementById("map"); // 지도를 표시할 div
+    let mapOption = {
+      center: new kakao.maps.LatLng(37.559023, 127.005296), // 지도의 중심좌표
+      level: 3, // 지도의 확대 레벨
+    };
 
-      var mapOption = {
-        center: new kakao.maps.LatLng(37.559023, 127.005296), // 지도의 중심좌표
-        level: 3, // 지도의 확대 레벨
+    // 지도를 생성합니다
+    let map = new kakao.maps.Map(mapContainer, mapOption);
+
+    // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+    let mapTypeControl = new kakao.maps.MapTypeControl();
+    map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+    // 지도 확대 축소를 제어할 수 있는 줌 컨트롤을 생성합니다
+    let zoomControl = new kakao.maps.ZoomControl();
+    map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+    // 기존 마커 제거
+    markers.forEach(marker => marker.setMap(null));
+    markers = [];
+
+    // 지도 영역을 설정하는 객체를 생성합니다
+    let bounds = new kakao.maps.LatLngBounds();
+
+    filteredPositions.forEach(position => {
+      // 마커를 생성합니다
+      let marker = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(position.latLng.lat, position.latLng.lng)
+      });
+
+      // 마커에 표시할 인포윈도우를 생성합니다 
+      let infowindow = new kakao.maps.InfoWindow({
+        content: position.content
+      });
+
+      // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록
+      kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
+      kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
+
+      markers.push(marker);
+
+      // bounds 객체에 마커의 좌표를 추가합니다
+      bounds.extend(new kakao.maps.LatLng(position.latLng.lat, position.latLng.lng));
+    });
+
+    // 검색 결과가 있을 경우, 해당 마커들의 위치가 모두 보이도록 지도 영역을 재설정합니다
+    if (filteredPositions.length > 0) {
+      map.setBounds(bounds);
+    }
+
+    // 인포윈도우를 표시하는 클로저를 만드는 함수 
+    function makeOverListener(map, marker, infowindow) {
+      return function () {
+        infowindow.open(map, marker);
       };
-
-      // 지도를 생성합니다
-      var map = new kakao.maps.Map(mapContainer, mapOption);
-
-      // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
-      var mapTypeControl = new kakao.maps.MapTypeControl();
-
-      // 지도에 컨트롤을 추가해야 지도위에 표시됩니다
-      // kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
-      map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
-
-      // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
-      var zoomControl = new kakao.maps.ZoomControl();
-      map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);   
-
-
-      for (var i = 0; i < dopositions.length; i ++) {
-          // 마커를 생성합니다
-          var marker = new kakao.maps.Marker({
-              map: map, // 마커를 표시할 지도
-              position: new kakao.maps.LatLng(dopositions[i].latLng.lat, dopositions[i].latLng.lng) // 마커의 위치
-          });
-
-          // 마커에 표시할 인포윈도우를 생성합니다 
-          var infowindow = new kakao.maps.InfoWindow({
-              content: dopositions[i].content // 인포윈도우에 표시할 내용
-          });
-
-          // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록
-          // 이벤트 리스너로는 클로저를 만들어 등록
-          // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
-          kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
-          kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
-      }
-
-      // 인포윈도우를 표시하는 클로저를 만드는 함수 
-      function makeOverListener(map, marker, infowindow) {
-          return function() {
-              infowindow.open(map, marker);
-          };
-      }
-      // 인포윈도우를 닫는 클로저를 만드는 함수
-      function makeOutListener(infowindow) {
-          return function() {
-              infowindow.close();
-          };
-      }
     }
-
-
-    catch (err) {
-      console.log(err);
+    // 인포윈도우를 닫는 클로저를 만드는 함수
+    function makeOutListener(infowindow) {
+      return function () {
+        infowindow.close();
+      };
     }
-    
-  });
+  }, [filteredPositions]);
 
   return (
     <div className="map_wrap">
       <div id="map"></div>
       <div id="menu_wrap" className="bg_white">
-      <DONav dopositions={dopositions}></DONav>
+        <DONav positions={filteredPositions} />
         <div id="pagination"></div>
       </div>
     </div>
   );
 };
+
 export default DopageMap;
