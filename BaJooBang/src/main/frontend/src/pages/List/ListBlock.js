5022b8c00ceda66b06c450a57c1d1ed960a5be6d
parkjoohyung <dochipjh@gmail.com>
2024-05-19 00:34:29 +0900

    [ADD] 신청조회 list 1차 완성

---
+++ BaJooBang/src/main/frontend/src/pages/List/ListBlock.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './ListBlock.css';
+
+function ListBlock({Num, Address, Person, Star}) {
+    return(
+        
+            
+                <div className='ListTitle'>
+                   <div className='ListTitleText_num'>{Num}</div> 
+                   <div className='ListTitleText_address'>{Address}</div>
+                   <div className='ListTitleText_person'>{Person}</div>
+                   <div className='ListTitleText_star'>{Star}</div>
+
+                </div>
+        
+        
+    )
+}
+
+export default ListBlock;
--- BaJooBang/src/main/frontend/src/pages/List/ListPage1.js
+++ BaJooBang/src/main/frontend/src/pages/List/ListPage1.js
@@ -1,16 +1,34 @@
 import React, { useState } from 'react';
 import './ListPage1.css';
+import ListBlock from './ListBlock';
 
 function ListPage1() {
+    const listData = [
+        { Num: '1', Address: '서울특별시 서초구 서초동', Person: '홍길동', Star: '4.8' },
+        { Num: '2', Address: '서울특별시 강남구 역삼동', Person: '이순신', Star: '4.9' },
+        { Num: '3', Address: '서울특별시 강동구 천호동', Person: '장보고', Star: '4.7' },
+        // 추가적인 데이터를 여기에 입력
+    ];
+    
     return(
         <div className='ListBackground'>
             <div className='pageTitle'>
                 발품 요청서 작성
             </div>
             <div className='ListBox'>
-                <div className='ListTitle'>
-                    
+                <div className='ListTitle' style={{backgroundColor: 'rgba(162, 197, 121, 0.3)'}}>
+                   <div className='ListTitleText_num'>번호</div> 
+                   <div className='ListTitleText_address'>주소</div>
+                   <div className='ListTitleText_person'>발품인</div>
+                   <div className='ListTitleText_star'>별점</div>
+
                 </div>
+                {listData.map((item, index) => (
+                    <React.Fragment key={index}>
+                        <ListBlock Num={item.Num} Address={item.Address} Person={item.Person} Star={item.Star} />
+                        <div className='ListLine' />
+                    </React.Fragment>
+                ))}
             </div>
         
         </div>
