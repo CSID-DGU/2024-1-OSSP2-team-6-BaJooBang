40ae36562a82e34c830259f4e2491cbb3e1ccc17
parkjoohyung <dochipjh@gmail.com>
2024-05-18 23:57:20 +0900

    [ADD] listpage 틀 추가

---
+++ BaJooBang/src/main/frontend/src/pages/List/ListPage1.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './ListPage1.css';
+
+function ListPage1() {
+    return(
+        <div className='ListBackground'>
+            <div className='pageTitle'>
+                발품 요청서 작성
+            </div>
+            <div className='ListBox'>
+                <div className='ListTitle'>
+                    
+                </div>
+            </div>
+        
+        </div>
+    )
+}
+
+export default ListPage1;
