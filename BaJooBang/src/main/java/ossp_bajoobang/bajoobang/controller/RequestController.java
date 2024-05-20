42fe0a596f5657d238d425726b36b738d8c4fe8f
deepBeom <i1t28ly@gmail.com>
2024-05-20 21:57:55 +0900

    요청 매물로부터 가까운 10명 선별

--- BaJooBang/src/main/java/ossp_bajoobang/bajoobang/controller/RequestController.java
+++ BaJooBang/src/main/java/ossp_bajoobang/bajoobang/controller/RequestController.java
@@ -9,10 +9,12 @@ import ossp_bajoobang.bajoobang.domain.House;
 import ossp_bajoobang.bajoobang.domain.Member;
 import ossp_bajoobang.bajoobang.dto.RequestDTO;
 import ossp_bajoobang.bajoobang.repository.HouseRepository;
+import ossp_bajoobang.bajoobang.repository.MemberRepository;
 import ossp_bajoobang.bajoobang.service.HouseService;
 import ossp_bajoobang.bajoobang.service.RequestService;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +23,7 @@ public class RequestController {
     private final HouseRepository houseRepository;
     private final RequestService requestService;
     private final HouseService houseService;
+    private final MemberRepository memberRepository;
 
     @PostMapping("/request-form")
     public String requestForm(@RequestBody RequestDTO requestDTO, HttpServletRequest request, @RequestParam Long house_id){
@@ -34,6 +37,13 @@ public class RequestController {
             House house = houseRepository.findByHouseId(house_id);
             // 새로운 요청서 저장
             requestService.saveRequest(requestDTO, member, house);
+
+            // 주어진 house의 위도와 경도로부터 가까운 회원 10명 검색
+            List<Member> nearbyMembers = memberRepository.findTop10MembersByDistance(house.getLatitude(), house.getLongitude());
+            nearbyMembers.forEach(m -> log.info("Member Address: " + m.getAddress()));
+
+
+
             return "GOOD";
         }
         else {
--- BaJooBang/src/main/java/ossp_bajoobang/bajoobang/repository/MemberRepository.java
+++ BaJooBang/src/main/java/ossp_bajoobang/bajoobang/repository/MemberRepository.java
@@ -1,10 +1,16 @@
 package ossp_bajoobang.bajoobang.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ossp_bajoobang.bajoobang.domain.Member;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
+
+    @Query(value = "SELECT *, (6371 * acos(cos(radians(:lat)) * cos(radians(latitude)) * cos(radians(longitude) - radians(:lng)) + sin(radians(:lat)) * sin(radians(latitude)))) AS distance FROM Member ORDER BY distance ASC LIMIT 10", nativeQuery = true)
+    List<Member> findTop10MembersByDistance(@Param("lat") double latitude, @Param("lng") double longitude);
 }
