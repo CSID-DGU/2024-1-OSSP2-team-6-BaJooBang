181aba282b2e86dfe7752b2add78276d43f4888c
youmeanit <umin030312@naver.com>
2024-05-22 20:44:12 +0900

    bell active 추가

--- BaJooBang/src/main/frontend/src/HelpPage/helpmap.js
+++ BaJooBang/src/main/frontend/src/HelpPage/helpmap.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 import './helpmap.css';
 import MypageMap from './kakaomap';
 import { ReactComponent as Closer } from '../components/images/closer.svg';
+import { ReactComponent as Bell } from '../components/images/bell.svg';
+import { ReactComponent as BellActive } from '../components/images/bellactive.svg';
 
 // 매물지도의 검색라인
-const SearchBar = ({ onFilterChange, onSearchChange, currentFilter }) => {
+const SearchBar = ({ onFilterChange, onSearchChange, currentFilter, hasNotification }) => {
   return (
     <div className="search">
       <label htmlFor="searchfor">매물 검색<span className='blank'></span>|</label><span className='blank'></span>
@@ -37,13 +39,18 @@ const SearchBar = ({ onFilterChange, onSearchChange, currentFilter }) => {
           전체
         </button>
       </div>
+      <div className="right_elements">
+        {hasNotification ? <BellActive /> : <Bell />}
+      </div>
     </div>
   );
 };
 
+
 const Helppage = () => {
   const [filter, setFilter] = useState('전체');
   const [search, setSearch] = useState('');
+  const [hasNotification, setHasNotification] = useState(false); // 알림 상태를 추적하기 위한 상태
 
   const handleFilterChange = (newFilter) => {
     setFilter(newFilter);
@@ -53,12 +60,20 @@ const Helppage = () => {
     setSearch(newSearch);
   };
 
+
+  // 실제 알림 확인 로직으로 교체 필요
+  const checkForNotifications = () => {
+    // 알림이 있는지 확인하는 로직
+    setHasNotification(true); // 알림이 있으면 true로 설정
+  };
+
   return (
     <div>
       <SearchBar 
         onFilterChange={handleFilterChange} 
         onSearchChange={handleSearchChange} 
         currentFilter={filter}
+        hasNotification={hasNotification}
       />
       <MypageMap filter={filter} search={search} />
     </div>
