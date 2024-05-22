a4c685a790cbbe118ee13832eaff4a3b731360d3
parkjoohyung <dochipjh@gmail.com>
2024-05-22 22:50:50 +0900

    [ADD] 리스트 페이지 페이지네이션 추가

--- BaJooBang/src/main/frontend/src/pages/List/ListPage1.js
+++ BaJooBang/src/main/frontend/src/pages/List/ListPage1.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './ListPage1.css';
 import ListBlock from './ListBlock1';
 import Modal from './Modal';
@@ -6,12 +6,24 @@ import Modal from './Modal';
 function ListPage1() {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [modalData, setModalData] = useState(null);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [currentItems, setCurrentItems] = useState([]);
+    const itemsPerPage = 10;
 
     const listData = [
         { Num: '1', Address: '서울특별시 서초구 서초동', Person: '홍길동', Star: '4.8' },
         { Num: '2', Address: '서울특별시 강남구 역삼동', Person: '이순신', Star: '4.9' },
         { Num: '3', Address: '서울특별시 강동구 천호동', Person: '장보고', Star: '4.7' },
-        // 추가적인 데이터를 여기에 입력
+        { Num: '4', Address: '서울특별시 강서구 화곡동', Person: '유관순', Star: '4.6' },
+        { Num: '5', Address: '서울특별시 마포구 합정동', Person: '안중근', Star: '4.5' },
+        { Num: '6', Address: '서울특별시 용산구 이태원동', Person: '김구', Star: '4.4' },
+        { Num: '7', Address: '서울특별시 성동구 성수동', Person: '김좌진', Star: '4.3' },
+        { Num: '8', Address: '서울특별시 종로구 혜화동', Person: '윤봉길', Star: '4.2' },
+        { Num: '9', Address: '서울특별시 강서구 화곡동', Person: '유관순', Star: '4.6' },
+        { Num: '10', Address: '서울특별시 마포구 합정동', Person: '안중근', Star: '4.5' },
+        { Num: '11', Address: '서울특별시 용산구 이태원동', Person: '김구', Star: '4.4' },
+        { Num: '12', Address: '서울특별시 성동구 성수동', Person: '김좌진', Star: '4.3' },
+        { Num: '13', Address: '서울특별시 종로구 혜화동', Person: '윤봉길', Star: '4.2' },
     ];
 
     const handleBlockClick = (data) => {
@@ -24,20 +36,34 @@ function ListPage1() {
         setModalData(null);
     };
 
+    // Update current items when currentPage changes
+    useEffect(() => {
+        const indexOfLastItem = currentPage * itemsPerPage;
+        const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+        setCurrentItems(listData.slice(indexOfFirstItem, indexOfLastItem));
+    }, [currentPage, listData]);
+
+    // Change page
+    const paginate = (pageNumber, event) => {
+        event.preventDefault();
+        setCurrentPage(pageNumber);
+    };
+
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(listData.length / itemsPerPage); i++) {
+        pageNumbers.push(i);
+    }
+
     return(
         <div className='ListBackground'>
-            {/* <div className='pageTitle'>
-                발품 요청서 작성
-            </div> */}
             <div className='ListBox'>
                 <div className='ListTitle' style={{backgroundColor: 'rgba(162, 197, 121, 0.3)'}}>
                    <div className='ListTitleText_num'>번호</div> 
                    <div className='ListTitleText_address'>주소</div>
                    <div className='ListTitleText_person'>발품인</div>
                    <div className='ListTitleText_star'>별점</div>
-
                 </div>
-                {listData.map((item, index) => (
+                {currentItems.map((item, index) => (
                     <React.Fragment key={index}>
                         <ListBlock 
                             Num={item.Num} 
@@ -50,6 +76,17 @@ function ListPage1() {
                     </React.Fragment>
                 ))}
             </div>
+            <nav>
+                <ul className='pagination'>
+                    {pageNumbers.map(number => (
+                        <li key={number} className='page-item'>
+                            <a onClick={(event) => paginate(number, event)} href='!#' className='page-link'>
+                                {number}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
             <Modal isOpen={isModalOpen} onClose={closeModal} data={modalData} />
         </div>
     )
--- BaJooBang/src/main/frontend/src/pages/List/ListPage2.js
+++ BaJooBang/src/main/frontend/src/pages/List/ListPage2.js
@@ -1,21 +1,47 @@
-import React, { useState } from 'react';
-import './ListPage1.css';
+import React, { useState, useEffect } from 'react';
+import './ListPage1.css'; // 페이지 스타일을 여기에 유지합니다.
 import ListBlock2 from './ListBlock2';
 import { Link } from 'react-router-dom';
 
 function ListPage2() {
+    const [currentPage, setCurrentPage] = useState(1);
+    const [currentItems, setCurrentItems] = useState([]);
+    const itemsPerPage = 10;
+
     const listData = [
         { Num: '1', Address: '서울특별시 서초구 서초동', month: '2000/130', date: '2024.03.29', request: '0', state: '요청 중' },
         { Num: '2', Address: '서울특별시 강남구 역삼동', month: '1000/50', date: '2024.03.28', request: '1', state: '요청 중' },
         { Num: '3', Address: '서울특별시 강동구 천호동', month: '100/23', date: '2024.03.24', request: '4', state: '매칭 완료' },
-        // 추가적인 데이터를 여기에 입력
+        { Num: '4', Address: '서울특별시 강서구 화곡동', month: '2000/130', date: '2024.03.22', request: '2', state: '요청 중' },
+        { Num: '5', Address: '서울특별시 마포구 합정동', month: '300/30', date: '2024.03.21', request: '3', state: '매칭 완료' },
+        { Num: '6', Address: '서울특별시 용산구 이태원동', month: '100/10', date: '2024.03.20', request: '1', state: '요청 중' },
+        { Num: '7', Address: '서울특별시 성동구 성수동', month: '500/50', date: '2024.03.19', request: '0', state: '요청 중' },
+        { Num: '8', Address: '서울특별시 종로구 혜화동', month: '400/40', date: '2024.03.18', request: '2', state: '매칭 완료' },
+        { Num: '9', Address: '서울특별시 서대문구 홍제동', month: '700/70', date: '2024.03.17', request: '4', state: '요청 중' },
+        { Num: '10', Address: '서울특별시 동작구 상도동', month: '1000/100', date: '2024.03.16', request: '3', state: '요청 중' },
+        { Num: '11', Address: '서울특별시 송파구 잠실동', month: '200/20', date: '2024.03.15', request: '2', state: '매칭 완료' },
+        { Num: '12', Address: '서울특별시 강남구 청담동', month: '1500/150', date: '2024.03.14', request: '1', state: '요청 중' },
+        { Num: '13', Address: '서울특별시 영등포구 여의도동', month: '2500/250', date: '2024.03.13', request: '0', state: '요청 중' },
     ];
+
+    useEffect(() => {
+        const indexOfLastItem = currentPage * itemsPerPage;
+        const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+        setCurrentItems(listData.slice(indexOfFirstItem, indexOfLastItem));
+    }, [currentPage, listData]);
+
+    const paginate = (pageNumber, event) => {
+        event.preventDefault();
+        setCurrentPage(pageNumber);
+    };
+
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(listData.length / itemsPerPage); i++) {
+        pageNumbers.push(i);
+    }
 
     return(
         <div className='ListBackground'>
-            {/* <div className='pageTitle'>
-                발품 요청서 작성
-            </div> */}
             <div className='ListBox'>
                 <div className='ListTitle' style={{backgroundColor: 'rgba(162, 197, 121, 0.3)'}}>
                    <div className='ListTitleText2'>번호</div> 
@@ -24,19 +50,27 @@ function ListPage2() {
                    <div className='ListTitleText2'>등록일</div>
                    <div className='ListTitleText2'>신청 수</div>
                    <div className='ListTitleText2'>상태</div>
-
                 </div>
-                {listData.map((item, index) => (
+                {currentItems.map((item, index) => (
                     <React.Fragment key={index}>
-                        <Link to='/matching'>
+                        <Link to='/matching' className='ListLinkNotLine'>
                             <ListBlock2 Num={item.Num} Address={item.Address} month={item.month} date={item.date} request={item.request} state={item.state} />
                         </Link>
-                        
                         <div className='ListLine' />
                     </React.Fragment>
                 ))}
             </div>
-        
+            <nav>
+                <ul className='pagination'>
+                    {pageNumbers.map(number => (
+                        <li key={number} className='page-item'>
+                            <a onClick={(event) => paginate(number, event)} href='!#' className='page-link'>
+                                {number}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
         </div>
     )
 }
