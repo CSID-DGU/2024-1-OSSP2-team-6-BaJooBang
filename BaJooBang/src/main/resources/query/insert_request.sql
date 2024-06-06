83180c55d830dd0ee68847aa3ff8fddd8eab3e4a
wsxchoi <wsxchoi@gmail.com>
2024-06-06 23:10:51 +0900

    PATCH_accept 에러 수정

--- BaJooBang/src/main/resources/query/insert_request.sql
+++ BaJooBang/src/main/resources/query/insert_request.sql
@@ -1,5 +1,5 @@
-insert into request(mold_living, mold_rest, mold_shoes, mold_veranda, mold_window, power_shower, power_wash, power_water, price_request, request_date, lighting, time_water)
-values (1, 0, 0, 0, 1, 1, 0, 2, 4000, '2024-04-14', "good", 32),
-(0, 1, 1, 0, 1, 2, 2, 2, 5000, '2024-04-17', "bad", 42);
+insert into request(mold_living, mold_rest, mold_shoes, mold_veranda, mold_window, power_shower, power_wash, power_water, price_request, request_date, lighting, time_water, member_id, house_id, status)
+values (1, 0, 0, 0, 1, 1, 0, 2, 4000, '2024-04-14', "good", 32, 1, 1, "매칭 전"),
+(0, 1, 1, 0, 1, 2, 2, 2, 5000, '2024-04-17', "bad", 42, 2, 1, "매칭 전");
 
 select * from request;
