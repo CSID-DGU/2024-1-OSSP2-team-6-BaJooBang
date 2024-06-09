01ebd46cb134efe599b82409d42a2ba7f70626a0
parkjoohyung <dochipjh@gmail.com>
2024-06-09 21:45:16 +0900

    [ADD] 매칭 api 연결 추가

--- BaJooBang/src/main/frontend/src/pages/List/ListPage4.js
+++ BaJooBang/src/main/frontend/src/pages/List/ListPage4.js
@@ -66,8 +66,12 @@ function ListPage4() {
                             <ListBlock4 Num={item.Num} Address={item.Address} Price={item.Price} Date={item.Date} Request_id={item.Request_id} State={item.State} />
                         </div>
                         :
-                        <Link to='/matching' className='ListLinkNotLine'>
-                            <ListBlock4 Num={item.Num} Address={item.Address} Price={item.Price} Date={item.Date} Request_id={item.Request_id} State={item.State} />
+                        <Link 
+                            to='/registered/matching' 
+                            state={{ fromPage: 'ListPage4', Request_id: item.Request_id }} 
+                            className='ListLinkNotLine'
+                        >
+                            <ListBlock4 Num={item.Num} Address={item.Address} Price={item.Price} State={item.State} Date={item.Date} Request_id={item.Request_id} />
                         </Link>
                         }
                         <div className='ListLine' />
