f24b2c57c5e801da624a10f9fd5676b68817e2b9
wsxchoi <wsxchoi@gmail.com>
2024-06-09 16:19:50 +0900

    사진 보이게 하기 2

--- BaJooBang/src/main/java/ossp_bajoobang/bajoobang/WebConfig.java
+++ BaJooBang/src/main/java/ossp_bajoobang/bajoobang/WebConfig.java
@@ -3,6 +3,7 @@ package ossp_bajoobang.bajoobang;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -12,5 +13,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(new ByteArrayHttpMessageConverter());
+    }
+
+    @Override
+    public void  addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/attach/images/**") // --1
+                .addResourceLocations("file:///Users/woosungchoi/study/file/"); //--2
     }
 }
