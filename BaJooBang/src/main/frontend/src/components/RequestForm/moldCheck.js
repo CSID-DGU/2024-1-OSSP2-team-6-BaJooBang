3d2388f6ac699e6967ec7140be5c9ad42447efb3
parkjoohyung <dochipjh@gmail.com>
2024-06-09 20:54:41 +0900

    곰팡이 수정

--- BaJooBang/src/main/frontend/src/components/RequestForm/moldCheck.js
+++ BaJooBang/src/main/frontend/src/components/RequestForm/moldCheck.js
@@ -3,8 +3,8 @@ import './moldCheck.css';
 
 function MoldCheck({ complete, savedState, onChange }) {
   const [checkedState, setCheckedState] = useState({
-    hasItem: savedState?.hasItem || false,
-    noItem: savedState?.noItem || false
+    hasItem: savedState?.hasItem || null,
+    noItem: savedState?.noItem || null
   });
 
   useEffect(() => {
@@ -45,7 +45,6 @@ function MoldCheck({ complete, savedState, onChange }) {
 
   return (
     <div className="checkbox-container">
-<<<<<<< HEAD
       {complete ?
         <>
           <label style={{display: 'flex', alignItems: 'center'}}>
@@ -91,29 +90,7 @@ function MoldCheck({ complete, savedState, onChange }) {
           </label>
           </>
         }
-      
-=======
-      <label style={{display: 'flex', alignItems: 'center'}}>
-        <input
-          type="checkbox"
-          name="hasItem"
-          checked={checkedState.hasItem}
-          onChange={handleOnChange}
-          disabled={complete}
-        />
-        <p style={{fontSize: '0.9vw'}}>있음</p>
-      </label>
-      <label style={{display: 'flex', alignItems: 'center'}}>
-        <input
-          type="checkbox"
-          name="noItem"
-          checked={checkedState.noItem}
-          onChange={handleOnChange}
-          disabled={complete}
-        />
-        <p style={{fontSize: '0.9vw'}}>없음</p>
-      </label>
->>>>>>> 1cd96d7 (요청 수정)
+    
     </div>
   );
 }
