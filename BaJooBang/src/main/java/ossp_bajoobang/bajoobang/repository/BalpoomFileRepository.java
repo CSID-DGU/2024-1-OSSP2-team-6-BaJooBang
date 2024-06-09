4d737c83a6ad33d994f74d839ab50131ae58373c
deepBeom <i1t28ly@gmail.com>
2024-06-09 20:01:04 +0900

    발품서 파일 get 수정

--- BaJooBang/src/main/java/ossp_bajoobang/bajoobang/repository/BalpoomFileRepository.java
+++ BaJooBang/src/main/java/ossp_bajoobang/bajoobang/repository/BalpoomFileRepository.java
@@ -3,10 +3,11 @@ package ossp_bajoobang.bajoobang.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ossp_bajoobang.bajoobang.domain.File;
+import ossp_bajoobang.bajoobang.domain.Request;
 
 import java.util.List;
 
 @Repository
 public interface BalpoomFileRepository extends JpaRepository<File, Long> {
-    List<File> findAll();
+    List<File> findByRequest(Request request);
 }
