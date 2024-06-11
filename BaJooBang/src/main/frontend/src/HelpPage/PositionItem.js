3cdc771a1facef17170d5dbbd7c237b7e4dd0ab9
parkjoohyung <dochipjh@gmail.com>
2024-06-11 21:08:10 +0900

    [DEL] box.css 삭제

--- BaJooBang/src/main/frontend/src/HelpPage/PositionItem.js
+++ BaJooBang/src/main/frontend/src/HelpPage/PositionItem.js
@@ -3,7 +3,6 @@ import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import './box.css';
 
 const PositionItem = ({ position, isLoggedIn }) => {
   const navigate = useNavigate();
